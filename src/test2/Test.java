148bae154bcdbd43a65cdc6b5445b323c659e95e
Antonio Aguilar Gomez <aaguil50@calpoly.edu>
2018-10-05 10:31:39 -0700

    Adding everything!

---
+++ src/test2/Test.java
@@ -0,0 +1,12 @@
+package test2;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Multiplying 2 and 2");
+		int result = mult(2,2);
+		System.out.println("result: " + result);
+	}
+	public static int mult(int x, int y) {
+		return x * y;
+	}
+}
